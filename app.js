beee1934adbe02dc70df8d43125cfdf573ce136c
Mikko Maja <mikmaj88@gmail.com>
2019-01-21 18:57:21 +0200

    Add argsv functionality and print location

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 require('dotenv').config()
 const request = require('request')
+const yargs = require('yargs')
 
+// Get the api key from the .env file
 const API_KEY = process.env.API_KEY
 
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv
+
+const address = encodeURIComponent(argv.a)
+
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=iivisniemenkuja%204%2002260%20espoo%20finland&key=${API_KEY}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`,
     json: true
 }, (error, response, body) => {
-    console.log(body)
+    // Print the address, latitude and longitude from the body
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
