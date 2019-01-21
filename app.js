ff99f8dadc28f6c4cf1e18e93f8a0d56f605c003
Mikko Maja <mikmaj88@gmail.com>
2019-01-21 19:48:43 +0200

    Refactor app.js => geocode.js and add callbacks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-const request = require('request')
 const yargs = require('yargs')
+
+const geocode = require('./geocode/geocode')
 
 // Get the api key from the .env file
 const API_KEY = process.env.API_KEY
@@ -18,23 +19,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv
 
-const address = encodeURIComponent(argv.a)
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`,
-    json: true
-}, (error, response, body) => {
-    // Print the address, latitude and longitude from the body
-    if (error) {
-        console.log('Unable to connect to Google servers.')
-    } else if (body.status === "ZERO_RESULTS") {
-        console.log('Invalid address.')
-    } else if (body.status === "OK") {
-        console.log(`Address: ${body.results[0].formatted_address}`)
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+geocode.geocodeAddress(argv.address, API_KEY, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unknown error')
+        console.log(JSON.stringify(results, undefined, 4))
     }
-
 })
