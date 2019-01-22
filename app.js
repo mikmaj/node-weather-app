d5a838ecf290a62702ad043cdd1a07537869565f
Mikko Maja <mikmaj88@gmail.com>
2019-01-22 15:23:10 +0200

    Add forecast.io weather query

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
+// require('dotenv').config()
+// const yargs = require('yargs')
+
+// const geocode = require('./geocode/geocode')
+
+// // Get the api key from the .env file
+// const API_KEY = process.env.API_KEY
+
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv
+
+// geocode.geocodeAddress(argv.address, API_KEY, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 4))
+//     }
+// })
 require('dotenv').config()
-const yargs = require('yargs')
+const request = require('request')
 
-const geocode = require('./geocode/geocode')
+const API_KEY_NEW = process.env.API_KEY_NEW
 
-// Get the api key from the .env file
-const API_KEY = process.env.API_KEY
-
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv
-
-geocode.geocodeAddress(argv.address, API_KEY, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+request({
+    url: `https://api.darksky.net/forecast/${API_KEY_NEW}/61.4452417,23.8341589?units=si`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log("Unable to connect to Forecast.io servers")
+    } else if (response.statusCode === 400) {
+        console.log("Api key, address or URL is invalid.")
+    } else if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature)
     } else {
-        console.log(JSON.stringify(results, undefined, 4))
+        console.log('Unable to fetch weather.')
     }
+
 })
