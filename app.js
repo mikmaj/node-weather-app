a3a69495660df83f891eee357f0e3002b5c73340
Mikko Maja <mikmaj88@gmail.com>
2019-01-22 17:05:30 +0200

    Add axios and rebuild the app to a single file

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const yargs = require('yargs')
-
-const geocode = require('./geocode/geocode')
-const weather = require('./weather/weather')
+const axios = require('axios')
 
 // Get the api keys from the .env file
 const LOCATION_API_KEY = process.env.LOCATION_API_KEY
@@ -21,21 +19,26 @@ const argv = yargs
     .alias('help', 'h')
     .argv
 
-geocode.geocodeAddress(argv.address, LOCATION_API_KEY, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+const encodedAddress = encodeURIComponent(argv.address)
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${LOCATION_API_KEY}`
+
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address.')
+    }
+    const lat = response.data.results[0].geometry.location.lat
+    const lng = response.data.results[0].geometry.location.lng
+    const weatherUrl = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/${lat},${lng}?units=si`
+    console.log(response.data.results[0].formatted_address)
+    return axios.get(weatherUrl)
+}).then((response) => {
+    const temperature = response.data.currently.temperature
+    const apparentTemperature = response.data.currently.apparentTemperature
+    console.log(`The current temperature is ${temperature}°C. It feels like ${apparentTemperature}°C`)
+}).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers.')
     } else {
-        console.log(results.address)
-        // Send the api key, location information and callbacks to weather.js
-        weather.getWeather(WEATHER_API_KEY, results.latitude, results.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(errorMessage)
-            } else {
-                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemp}.`)
-            }
-        })
+        console.log(e.message)
     }
 })
-
-
-
